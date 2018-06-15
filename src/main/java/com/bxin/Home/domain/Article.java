3f1792f151e6d02f0d1942a79ed2eeab91a05f4f
ElementCraft <976228284@qq.com>
2018-06-15 18:44:23 +0800

    fix

---
+++ src/main/java/com/bxin/Home/domain/Article.java
@@ -0,0 +1,33 @@
+package com.bxin.Home.domain;
+
+
+import com.bxin.Home.tools.entity.BasePojo;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.ZonedDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "article")
+public class Article extends BasePojo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long navId;
+
+    private String title;
+
+    /**
+     * 邮箱
+     */
+    private String email;
+
+    private String content;
+}
