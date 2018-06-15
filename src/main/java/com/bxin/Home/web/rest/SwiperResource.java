4e190f6fcdbaf4ac73492bbf5c026808a8a19d10
ElementCraft <976228284@qq.com>
2018-06-15 16:14:18 +0800

    fix

--- src/main/java/com/bxin/Home/web/rest/SwiperResource.java
+++ src/main/java/com/bxin/Home/web/rest/SwiperResource.java
@@ -3,10 +3,13 @@ package com.bxin.Home.web.rest;
 import com.bxin.Home.domain.Swiper;
 import com.bxin.Home.service.SwiperService;
 import com.bxin.Home.tools.entity.Result;
+import com.bxin.Home.tools.utils.UploadUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -38,5 +41,24 @@ public class SwiperResource {
     @DeleteMapping("/del")
     public ResponseEntity<Result> del(Long id) {
         return ResponseEntity.ok(swiperService.del(id));
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<Result> uploadIcon(MultipartFile file){
+        Result dto = null;
+        UploadUtil.UploadLimit limit = new UploadUtil.UploadLimit();
+        //limit.setSavePath("uploads" + File.separator + "icons" + File.separator);
+        limit.setMaxSize(1024L * 1024L);
+        limit.setAllowExt(new ArrayList<String>(){{
+            add("jpg");
+            add("jpeg");
+            add("png");
+            add("bmp");
+            add("gif");
+            add("webp");
+        }});
+
+        dto = UploadUtil.checkAndSave(file, limit);
+        return ResponseEntity.ok(dto);
     }
 }
